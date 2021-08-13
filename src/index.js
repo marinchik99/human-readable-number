82ee15ca33a667e378f2d90d988c42918714205d
marinchik99 <marina_1999@tut.by>
2021-08-13 13:10:11 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function toReadable (number) {
-  
+    let arr = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    let arr1 = ['zero','ten','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number < 20){
+        return arr[number];
+    }
+    else if ( number <100 && number >= 20){
+        let res = number.toString().split('');
+        return arr1[res[0]]+' '+arr[res[1]];
+    }
+    else if (number >= 100){
+        let res = number.toString().split('');
+        return arr[res[0]]+' hundred '+arr1[res[1]]+' '+arr[res[2]];
+    }
 }
+
